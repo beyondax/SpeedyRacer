34c538b53fd8e81fde1fb94d68e38ecd33f8e405
beyondax <antonyxdark@gmail.com>
2019-12-09 03:52:50 +0300

    F1.

--- app/src/main/java/com/example/speedyracer/MySpeedView.java
+++ app/src/main/java/com/example/speedyracer/MySpeedView.java
@@ -1,6 +1,8 @@
 package com.example.speedyracer;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
@@ -10,9 +12,12 @@ import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -28,21 +33,38 @@ public class MySpeedView extends View {
     private static final float FONT_SIZE = 64f;
     private static final float START_ANGLE = -180f;
     private static final float MAX_PROGRESS = 200f;
+    private static int[] mArcColorArray = new int[]{
+            Color.GREEN, Color.YELLOW, Color.RED
+    };
 
     private static final Paint TEXT_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final Paint ARC_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final Paint ARROW_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final Paint CIRCLE_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    private static final int MAX_SPEED = 200;
-    private static final int MIN_SPEED = 0;
+
     private static final float MAX_ANGLE = 180;
 
     private RectF mArcBounds;
     private Rect mTextBounds = new Rect();
-    private Path mArrowPath;
+
 
     private int mProgress;
+    private int mMaxSpeed;
+    private int mMinSpeed;
+
+    @ColorInt
+    private int mColorArrow;
+
+    public MySpeedView(Context context) {
+        this(context, null, 0);
+
+
+    }
+
+    public MySpeedView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
     public int getProgress() {
         return mProgress;
@@ -54,22 +76,33 @@ public class MySpeedView extends View {
         invalidate();
     }
 
-    public MySpeedView(Context context) {
-        super(context);
-        init();
-    }
-
-    public MySpeedView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
-
     public MySpeedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context, attrs);
     }
 
-    private void init() {
+    @Nullable
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState savedState = new SavedState(superState);
+        savedState.mProgress = this.mProgress;
+        return savedState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        SavedState savedState = (SavedState) state;
+        super.onRestoreInstanceState(savedState.getSuperState());
+        this.mProgress = savedState.mProgress;
+        invalidate();
+    }
+
+    private void init(@NonNull Context context, @Nullable AttributeSet attrs) {
+
+        setSaveEnabled(true);
+
+        extractAttributes(context, attrs);
 
         mArcBounds = new RectF(X_LEFT, Y_TOP, X_RIGHT, Y_BOTTOM);
 
@@ -78,7 +111,7 @@ public class MySpeedView extends View {
         ARC_PAINT.setStyle(Paint.Style.STROKE);
         ARC_PAINT.setStrokeCap(Paint.Cap.ROUND);
 
-        ARROW_PAINT.setColor(Color.BLACK);
+        ARROW_PAINT.setColor(mColorArrow);
         ARROW_PAINT.setStrokeWidth(STROKE_WIDTH / 10f);
         ARROW_PAINT.setStyle(Paint.Style.FILL_AND_STROKE);
 
@@ -86,7 +119,7 @@ public class MySpeedView extends View {
         TEXT_PAINT.setStyle(Paint.Style.FILL);
         TEXT_PAINT.setTextSize(FONT_SIZE);
 
-        CIRCLE_PAINT.setColor(Color.BLACK);
+        CIRCLE_PAINT.setColor(mColorArrow);
         CIRCLE_PAINT.setStrokeWidth(STROKE_WIDTH / 10f);
         CIRCLE_PAINT.setStyle(Paint.Style.FILL_AND_STROKE);
 
@@ -99,7 +132,7 @@ public class MySpeedView extends View {
         final float x = mArcBounds.centerX();
         final float y = mArcBounds.centerY();
         arrowPath.moveTo(x, y - STROKE_WIDTH / 5f);
-        arrowPath.lineTo(x - 3f * X_LEFT, y);
+        arrowPath.lineTo(x - X_RIGHT / 2f, y);
         arrowPath.lineTo(x, y + STROKE_WIDTH / 5f);
         arrowPath.close();
 
@@ -116,11 +149,22 @@ public class MySpeedView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        final String maxProgressString = formatString(mProgress);
+        getTextBounds(maxProgressString);
+        float desiredWidth = Math.max(mTextBounds.width(), getSuggestedMinimumWidth()) + getPaddingLeft() + getPaddingRight();
+        float desiredHeight = Math.max(mTextBounds.height(), getSuggestedMinimumHeight()) + getPaddingTop() + getPaddingBottom();
+        int desiredSize = (int) (Math.max(desiredHeight, desiredWidth));
+        final int resolvedWidth = resolveSize(desiredSize, widthMeasureSpec);
+        final int resolvedHeight = resolveSize(desiredSize, heightMeasureSpec);
+        setMeasuredDimension(resolvedWidth, resolvedHeight);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        final int size = Math.min(w, h);
+        mArcBounds = new RectF(getPaddingLeft() + STROKE_WIDTH / 2, STROKE_WIDTH / 2 + getPaddingTop(), size - STROKE_WIDTH / 2 - getPaddingRight(), size - STROKE_WIDTH / 2 - getPaddingBottom());
+
     }
 
     @Override
@@ -138,7 +182,7 @@ public class MySpeedView extends View {
     }
 
     private Shader createShader() {
-        LinearGradient shader = new LinearGradient(X_LEFT, Y_BOTTOM, X_RIGHT, Y_BOTTOM, new int[]{Color.GREEN, Color.YELLOW, Color.YELLOW, Color.RED}, null, Shader.TileMode.CLAMP);
+        LinearGradient shader = new LinearGradient(X_LEFT, Y_BOTTOM, X_RIGHT, Y_BOTTOM, mArcColorArray, null, Shader.TileMode.CLAMP);
         return shader;
 
     }
@@ -151,22 +195,22 @@ public class MySpeedView extends View {
         final String progressString = formatString(mProgress);
         getTextBounds(progressString);
         float x = mArcBounds.width() / 2f - mTextBounds.width() / 2f - mTextBounds.left + mArcBounds.left;
-        float y = mArcBounds.bottom / 2f + mTextBounds.height() + mTextBounds.bottom + mArcBounds.top;
+        float y = mArcBounds.bottom / 2f + mTextBounds.height() + mTextBounds.bottom + mArcBounds.top + RADIUS;
         canvas.drawText(progressString, x, y, TEXT_PAINT);
     }
 
     private void drawTextMin(Canvas canvas) {
-        final String minSpeedText = formatString(MIN_SPEED);
+        final String minSpeedText = formatString(mMinSpeed);
         getTextBounds(minSpeedText);
-        float x = mArcBounds.width() / 2f - mTextBounds.width() - mTextBounds.right / 2f - mArcBounds.left;
+        float x = mArcBounds.left + mTextBounds.width() / 2f - mTextBounds.right / 2f - mArcBounds.left;
         float y = mArcBounds.bottom / 2f + mTextBounds.height() + mTextBounds.bottom + mArcBounds.top;
         canvas.drawText(minSpeedText, x, y, TEXT_PAINT);
     }
 
     private void drawTextMax(Canvas canvas) {
-        final String maxSpeedText = formatString(MAX_SPEED);
+        final String maxSpeedText = formatString(mMaxSpeed);
         getTextBounds(maxSpeedText);
-        float x = mArcBounds.width() / 2f + mTextBounds.width() / 2f + mTextBounds.right / 2f + mArcBounds.left;
+        float x = mArcBounds.right / 2f + mTextBounds.width() / 2f + mTextBounds.right / 2f + mArcBounds.left / 2f;
         float y = mArcBounds.bottom / 2f + mTextBounds.height() + mTextBounds.bottom + mArcBounds.top;
         canvas.drawText(maxSpeedText, x, y, TEXT_PAINT);
     }
@@ -176,16 +220,51 @@ public class MySpeedView extends View {
     }
 
 
-//    private void extractAttributes(@NonNull Context context, @Nullable AttributeSet attrs) {
-//        if (attrs != null) {
-//            final Resources.Theme theme = context.getTheme();
-//            final TypedArray typedArray = theme.obtainStyledAttributes(attrs, R.styleable.MySpeedView, 0, R.style.MySpeedViewDefault);
-//            try {
-//                mProgress = typedArray.getInt(R.styleable.MySpeedView_progress, 0);
-//                mShaderColor = typedArray.getColor(R.styleable.MySpeedView_shader_color, Color.GREEN);
-//            } finally {
-//                typedArray.recycle();
-//            }
-//        }
-//    }
+    private void extractAttributes(@NonNull Context context, @Nullable AttributeSet attrs) {
+        if (attrs != null) {
+            final Resources.Theme theme = context.getTheme();
+            final TypedArray typedArray = theme.obtainStyledAttributes(attrs, R.styleable.MySpeedView, 0, R.style.MySpeedViewDefault);
+            try {
+                mProgress = typedArray.getInt(R.styleable.MySpeedView_progress, 0);
+                mMinSpeed = typedArray.getInt(R.styleable.MySpeedView_minSpeed, 0);
+                mMaxSpeed = typedArray.getInt(R.styleable.MySpeedView_maxSpeed, 200);
+                mColorArrow = typedArray.getColor(R.styleable.MySpeedView_colorArrow, 0);
+                int shaderColors = typedArray.getResourceId(R.styleable.MySpeedView_shaderColors, 0);
+                mArcColorArray = typedArray.getResources().getIntArray(shaderColors);
+            } finally {
+                typedArray.recycle();
+            }
+        }
+    }
+
+    static class SavedState extends BaseSavedState {
+
+        public static final Parcelable.Creator<SavedState> CREATOR
+                = new Parcelable.Creator<SavedState>() {
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+        int mProgress;
+
+
+        public SavedState(Parcel source) {
+            super(source);
+            mProgress = source.readInt();
+        }
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeInt(mProgress);
+        }
+    }
 }
